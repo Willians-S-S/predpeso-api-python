1b5c96c29993dae2a1004b02366118ecceef1742
Willians-S-S <09willians@gmail.com>
2025-02-27 11:50:25 -0300

    Feat: adicionar a rota da fazenda

--- src/predpeso/app.py
+++ src/predpeso/app.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from predpeso.routes.user_router import user_router
+from predpeso.routes.farm_router import farm_router
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(farm_router)
 
 @app.get("/")
 def get_test():
