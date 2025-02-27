6c035dff14cc4cded7f7731208e3ae8e355a003a
Willians-S-S <09willians@gmail.com>
2025-02-27 10:58:48 -0300

    Feat: add schemas de farm

---
+++ src/predpeso/schemas/farm_schemas.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class FarmRequest(BaseModel):
+    name: str | None = None
+    description: str | None = None
+    animal_quantity: int | None = None
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
+    user_id: str | None = None
+
+class FarmResponse(BaseModel):
+    id: str | None = None
+    name: str | None = None
+    description: str | None = None
+    animal_quantity: int | None = None
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
+    user_id: str | None = None
+
+class FarmUpdate(BaseModel):
+    name: str | None = None
+    description: str | None = None
+    animal_quantity: int | None = None
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
+    user_id: str | None = None
