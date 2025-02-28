ef0e1fe15dbd8335ac95e0959f1f3a5217332d04
Willians-S-S <09willians@gmail.com>
2025-02-28 16:18:38 -0300

    Feat: add rotas do animal

--- src/predpeso/app.py
+++ src/predpeso/app.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from predpeso.routes.user_router import user_router
 from predpeso.routes.farm_router import farm_router
+from predpeso.routes.animal_router import animal_router
 
 app = FastAPI()
 
 app.include_router(user_router)
 app.include_router(farm_router)
+app.include_router(animal_router)
 
 @app.get("/")
 def get_test():
