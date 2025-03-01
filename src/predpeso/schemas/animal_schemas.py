5992a244753db3db9c78f11798baa967ca6616fe
Willians-S-S <09willians@gmail.com>
2025-03-01 00:26:42 -0300

    Feat: add histórico do peso do animal

--- src/predpeso/schemas/animal_schemas.py
+++ src/predpeso/schemas/animal_schemas.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel
 from datetime import datetime
 
+from predpeso.schemas.history_schemas import HistoryResponse
+
 class AnimalRequest(BaseModel):
     name: str | None = None
     breed: str | None = None
     age: int | None = None
+    image_url: str | None = None
     gender: str | None = None
     health_condition: str | None = None
     farm_id: str 
@@ -15,11 +18,13 @@ class AnimalResponse(BaseModel):
     breed: str | None = None
     age: int | None = None
     gender: str | None = None
+    image_url: str | None = None
     health_condition: str | None = None
     current_weight: float | None = None
     created_at: datetime | None = None
     updated_at: datetime | None = None
     farm_id: str 
+    historys: list[HistoryResponse]
 
 class AnimalUpdate(BaseModel):
     id: str 
