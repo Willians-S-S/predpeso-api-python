4bb70a0ae2d9c93266dbeccb4a1a801699c25982
Willians-S-S <09willians@gmail.com>
2025-03-01 00:28:03 -0300

    Feat: Histórico e inferência do peso fictício

--- src/predpeso/services/animal_service.py
+++ src/predpeso/services/animal_service.py
@@ -3,9 +3,9 @@ from fastapi import HTTPException
 from fastapi import status
 from sqlalchemy.orm import Session
 import uuid
-from random import randint
+from random import random
 
-from predpeso.models.models import AnimalModel, FarmModel
+from predpeso.models.models import AnimalModel, FarmModel, History
 from predpeso.schemas.animal_schemas import AnimalRequest, AnimalResponse, AnimalUpdate
 
 ENTENY = "Animal"
@@ -24,11 +24,14 @@ class AnimalService:
                                 detail="Fazenda não encontrado.")
         
         date_created_and_updated = datetime.now()
-        current_weight = randint(1, 60)
-        
+        current_weight = random() * 100
+
         animal_on_db = AnimalModel(**animal.model_dump(), id=str(uuid.uuid4()), created_at=date_created_and_updated, updated_at=date_created_and_updated, current_weight=current_weight)
 
         self.db_session.add(animal_on_db)
+
+        self.add_history(weight=animal_on_db.current_weight, created_at=date_created_and_updated, image_url=animal_on_db.image_url, animal_id=animal_on_db.id)
+
         self.db_session.commit()
 
         return animal_on_db
@@ -48,6 +51,26 @@ class AnimalService:
         animals_on_db = self.db_session.query(AnimalModel).all()
 
         return animals_on_db
+    
+    def inference(self, animal_id: str, image_url: str):
+        animal_on_db = self.db_session.query(AnimalModel).filter_by(id = animal_id).first()
+
+        if not animal_on_db:
+             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                                detail=f"{ENTENY} não encontrado.")
+        
+        date_updated = datetime.now()
+        current_weight = random() * 100
+
+        animal_on_db.updated_at = date_updated
+        animal_on_db.current_weight = current_weight
+        animal_on_db.image_url = image_url
+
+        self.add_history(weight=animal_on_db.current_weight, created_at=date_updated, image_url=animal_on_db.image_url, animal_id=animal_on_db.id)
+
+        self.db_session.commit()
+
+        return animal_on_db
     
     def update(self, animal: AnimalUpdate, animal_id: str):
         animal_on_db = self.db_session.query(AnimalModel).filter_by(id = animal_id).first()
@@ -78,5 +101,7 @@ class AnimalService:
 
         return {status.HTTP_204_NO_CONTENT: f"{ENTENY} deletado com sucesso."}
 
-        
-    
+
+    def add_history(self, weight: float, created_at: datetime, image_url: str, animal_id: str):
+        history = History(id=str(uuid.uuid4()), weight=weight, created_at=created_at, image_url=image_url, animal_id=animal_id)
+        self.db_session.add(history)
