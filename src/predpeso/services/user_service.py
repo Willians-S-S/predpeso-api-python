ea4ce08260a03c24307458cc459ec15657a71a90
Willians-S-S <09willians@gmail.com>
2025-03-02 19:28:10 -0300

    Feat: add encriptar a senha e gerar o token

--- src/predpeso/services/user_service.py
+++ src/predpeso/services/user_service.py
@@ -1,11 +1,14 @@
 from datetime import datetime
-from fastapi import HTTPException
-from fastapi import status
+from fastapi import HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm 
 from sqlalchemy.orm import Session
 import uuid
 
 from predpeso.models.models import UserModel
 from predpeso.schemas.user_schemas import UserRequest, UserResponse, UserUpdate
+from predpeso.security.password_hash import get_password_hash, verify_password
+from predpeso.security.jwt_token import create_acess_token
+
 class UserService:
 
     def __init__(self, db_session: Session) -> None:
@@ -17,9 +20,20 @@ class UserService:
             .first()
         
         if(user_on_db):
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Usuário já foi cadastrado.")
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Email já foi cadastrado.")
+
+        del user_on_db
+
+        user_on_db = self.db_session.query(UserModel)\
+            .filter_by(cpf = user.cpf)\
+            .first()
+        
+        if(user_on_db):
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="CPF já foi cadastrado.")
         
         date_created_and_updated = datetime.now()
+
+        user.password = get_password_hash(user.password)
         
         user_on_db = UserModel(**user.model_dump(), id=str(uuid.uuid4()), created_at=date_created_and_updated, updated_at=date_created_and_updated)
 
@@ -55,6 +69,8 @@ class UserService:
         for fild, value in user.model_dump().items():
             if value:
                 print(fild, value)
+                if fild == "password":
+                    value = get_password_hash(value)
                 setattr(user_on_db, fild, value)
 
         self.db_session.commit()
@@ -74,5 +90,13 @@ class UserService:
 
         return {status.HTTP_204_NO_CONTENT: "Usuário deletado com sucesso."}
 
+    def login(self, form_data: OAuth2PasswordRequestForm):
+        user_on_db = self.db_session.query(UserModel).filter_by(username = form_data.username).first()
+
+        if not user_on_db or not verify_password(form_data.password, user_on_db.password):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username ou senha incorretos.")
         
+        access_token = create_acess_token(data={"sub": user_on_db.username})    
+
+        return {"access_token": access_token, "token_type": "bearer"}
     
